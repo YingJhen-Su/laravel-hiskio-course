a2b9e27ad406166e1abcac06d226c451508a854d
YingJhen-Su <clearsky0330@gmail.com>
2021-08-29 09:21:53 +0000

    [mod] add route resource of products

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/', 'ProductController@create');
+Route::resource('products', 'ProductController');
+
+//Route::group([
+//  'middleware' => ['checkValidIp'],
+//  'prefix'     => 'web',
+//  'namespace'  => 'Web'
+//], function() {
+//  Route::get('/index', 'HomeController@index');
+//  Route::post('/print', 'HomeController@print');
+//});
