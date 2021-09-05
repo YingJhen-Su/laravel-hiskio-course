d70184403ce79b57307ee46bbaa5765e8982d12e
YingJhen-Su <clearsky0330@gmail.com>
2021-09-05 14:18:12 +0000

    [mod] 使用passport製作會員註冊登入api

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,12 @@ Route::group(['middleware' => 'check.dirty'], function() {
 //Route::resource('products', 'ProductController');
 Route::resource('carts', 'CartController');
 Route::resource('cart-items', 'CartItemController');
+Route::post('signup', 'AuthController@signup');
+Route::post('login', 'AuthController@login');
+Route::group(['middleware' => 'auth:api'], function () {
+  Route::get('user', 'AuthController@user');
+  Route::get('logout', 'AuthController@logout');
+});
 
 //Route::group([
 //  'middleware' => ['checkValidIp'],
